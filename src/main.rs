71093905e9ec7726f7b474a7737aa46aa44046c5
gwirn <71886945+gwirn@users.noreply.github.com>
2024-06-21 11:09:56 +0100

    added different project detections

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use file_search::*;
 use format::*;
+use std::collections::HashMap;
 use std::env;
+use std::fs;
+use std::process::Command;
 use std::usize;
 use terminal_size::{terminal_size, Height, Width};
 use utils::*;
@@ -9,12 +12,45 @@ mod file_search;
 mod format;
 mod utils;
 
+fn is_proj(pwd: &str, file_ending: &str) -> bool {
+    let mut yes_proj = false;
+    if let Ok(files) = fs::read_dir(pwd) {
+        for f in files.into_iter() {
+            if let Ok(file_name) = f {
+                if let Some(file_ext) = file_name.path().extension().and_then(|x| x.to_str()) {
+                    yes_proj = file_ext == file_ending;
+                    if yes_proj {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    yes_proj
+}
+fn proj_version(com: &str, arg: &str) -> String {
+    match Command::new(com).arg(arg).output() {
+        Ok(out) => match String::from_utf8(out.stdout) {
+            Ok(o) => o,
+            Err(_) => "".to_string(),
+        },
+        Err(_) => "".to_string(),
+    }
+}
 fn main() {
+    // let file_extensions = HashMap::from([("py", "python"), ("rs", "rust"), ("go", "go")]);
+    let projects = HashMap::from([
+        ("python", ("py", ["python", "--version"])),
+        ("rust", ("rs", ["rustc", "--version"])),
+        ("go", ("go", ["go", "version"])),
+    ]);
     let orange = 208;
     let grey = 245;
     let blue = 24;
     let green = 65;
     let red = 9;
+    let turquoise = 45;
+    let carb_orange = 166;
     env::set_var("CLICOLOR_FORCE", "1");
 
     let shell = get_shell();
@@ -36,13 +72,6 @@ fn main() {
         Some((Width(ww), Height(_))) => ww.into(),
         None => 0,
     };
-
-    let mut py = "".to_string();
-    if !conda.is_empty() {
-        py = get_filename(conda);
-    } else if !venv.is_empty() {
-        py = get_filename(venv);
-    }
 
     let mut emoji_space = 0;
     let mut git_info = git_branch();
@@ -72,18 +101,77 @@ fn main() {
         path_string,
         color_and_esc(&git_info, &shell, grey),
     );
-    let path_len = ssh_string.len() + path_string.len() + 1 + git_info.len() - emoji_space;
-    let pylen = py.len();
 
+    let mut py = "".to_string();
+    if !conda.is_empty() {
+        py = get_filename(conda);
+    } else if !venv.is_empty() {
+        py = get_filename(venv);
+    }
+
+    let mut proj_len = py.len();
+    let mut proj_string: String = "".to_string();
+    for (p, c) in projects.into_iter() {
+        match p {
+            "python" => {
+                let (ending, com) = c;
+                if is_proj(&pwd, ending) {
+                    let proj_raw = proj_version(com[0], com[1]);
+                    let proj_split: Vec<_> = proj_raw.trim().split(' ').collect();
+                    if proj_split.len() == 2 {
+                        let pre_ps = format!("🐍{}", proj_split[1]);
+                        proj_len += pre_ps.len();
+                        proj_string =
+                            format!("{proj_string} {} ", color_and_esc(&pre_ps, &shell, blue));
+                    }
+                }
+            }
+            "rust" => {
+                let (ending, com) = c;
+                if is_proj(&pwd, ending) {
+                    let proj_raw = proj_version(com[0], com[1]);
+                    let proj_split: Vec<_> = proj_raw.trim().split(' ').collect();
+                    if proj_split.len() == 4 {
+                        let pre_ps = format!("🦀{}", proj_split[1]);
+                        proj_len += pre_ps.len();
+                        proj_string = format!(
+                            "{proj_string} {} ",
+                            color_and_esc(&pre_ps, &shell, carb_orange)
+                        );
+                    }
+                }
+            }
+            "go" => {
+                let (ending, com) = c;
+                if is_proj(&pwd, ending) {
+                    let proj_raw = proj_version(com[0], com[1]);
+                    let proj_split: Vec<_> = proj_raw.trim().split(' ').collect();
+                    if proj_split.len() == 4 {
+                        let pre_ps = format!("🐿️{}", proj_split[2]);
+                        proj_len += pre_ps.len();
+                        proj_string = format!(
+                            "{proj_string} {} ",
+                            color_and_esc(&pre_ps, &shell, turquoise)
+                        );
+                        emoji_space += 4
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+
+    let path_len = ssh_string.len() + path_string.len() + 1 + git_info.len() - emoji_space;
     let mut padding_size = 1;
-    if w >= (path_len + pylen) {
-        padding_size = w - path_len - pylen;
+    if w >= (path_len + proj_len) {
+        padding_size = w - path_len - proj_len;
     };
     let padding = vec![" "; padding_size].join("");
     path = format!(
-        "{}{}{}\n{}",
+        "{}{}{}{}\n{}",
         path,
         padding,
+        proj_string,
         color_and_esc(&py, &shell, blue),
         color_and_esc("»", &shell, orange)
     );
